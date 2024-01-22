cf297c2b234976cba8bcd2e3a8ea39580020f7fe
RedRossum <redrossum@gmail.com>
2024-01-22 22:40:33 +0300

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout, stdin } = process;
+
+const pathFile = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(pathFile);
+
+stdout.write('Please, enter your text.\n');
+
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  }
+  output.write(data);
+});
+
+process.on('exit', () => stdout.write('Exited from the program.'));
+process.on('SIGINT', process.exit);
